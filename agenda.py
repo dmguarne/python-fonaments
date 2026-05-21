8e3dd508a2d7a539a8ebaaf4e2e8e62bf9d1cff7
David <dmguarne@gmail.com>
2026-05-21 14:04:02 +0200

    Afegit agenda.py

---
+++ agenda.py
@@ -0,0 +1,4 @@
+## Crear una llista de diccionaris — cada element és un diccionari amb nom, edat i ciutat
+## Afegir 3 contactes a la llista
+## Mostrar tots els contactes numerats amb una funció mostrar_agenda(agenda)
+## Permetre a l'usuari cercar un contacte pel nom i mostrar totes les seves dades si el troba
