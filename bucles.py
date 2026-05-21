b6b82368df5656dfa7e2ba0d4ef25bda23970485
David <dmguarne@gmail.com>
2026-05-21 10:22:46 +0200

    Actualització exercici bucles

--- bucles.py
+++ bucles.py
@@ -5,9 +5,8 @@ while valor < 1 or valor > 10:
     print("El número ha d'estar entre 1 i 10")
     valor = int(input("Introdueix un número del 1 al 10 i prem Enter: "))
 for numero in range(1, 11):
-    
     print(f"{valor} x {numero} = {valor * numero}")
-    if int(valor * numero) / 2 == 1:
-        parells = + 1
-
-print(f"Hi ha un total de {parells} números parells")
+    if (valor * numero) % 2 == 0:
+        parells = parells + 1
+senars = 10 - parells
+print(f"Hi ha un total de {parells} números parells i {senars} números senars")
