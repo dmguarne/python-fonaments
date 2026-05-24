5506f7d7b36f8f183b42eb96e97e3dfb345b770d
David <dmguarne@gmail.com>
2026-05-24 20:54:10 +0200

    scraping_02.py que busca el tipus, títol i url de la imatge del dia de la NASA

---
+++ scraping_02.py
@@ -0,0 +1,11 @@
+import requests
+
+try:
+    response = requests.get("https://api.nasa.gov/planetary/apod?api_key=lGLvZyxLU6WX1fCDRnSnF5zUge9VxoehiVHTA2kB") ## Fa la petició al servidor i l'emmagatzema en text a la variable response.
+    if response.status_code == 200: ## Si la resposta del servidor és positiva, executa el codi.
+        dades = response.json() ## Converteix el text de la resposta del servidor a Python.
+        print(f"La imatge del dia és {dades['media_type']}, el títol és {dades['title']} i l'enllaç és {dades["url"]}") ## Imprimeix les claus tipus, títol i url.
+    else:
+        print(response.status_code) ## Si la resposta del servidor no és 200, informa del codi de resposta.
+except ConnectionError: ## Si la connexió amb el servidor ha fallat, informa.
+    print("No s'ha pogut connectar amb el servidor")
