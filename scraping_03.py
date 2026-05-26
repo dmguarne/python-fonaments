9985453b60714ca866ee11eba7812875906ac20f
David <dmguarne@gmail.com>
2026-05-26 13:55:26 +0200

    A scraping03, s'ha afegit un diccionari per traduir les puntuacions a int.

--- scraping_03.py
+++ scraping_03.py
@@ -2,52 +2,44 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-try:
-    with open("llibres.json", "r") as fitxer: ## Obre llibres.json i el llegeix a fitxer.
-        llibres = json.load(fitxer) ## Estableix la variable llibres, que emmagatzema el contingut del fitxer
-except FileNotFoundError:
-    llibres = []
-except json.JSONDecodeError:
-    print("El fitxer llibres.json està corromput.")
-    llibres = []
-
-
-
-response = requests.get("https://books.toscrape.com")
-soup = BeautifulSoup(response.text, "html.parser")
-elements = soup.find_all("article", class_="product_pod") # Retorna una llista amb tots els objectes "article".
-
-print("Llibres:\n")
-for i, article in enumerate(elements, start=1): # Recorre cada element de la llista
-    llibre = article.find("a", title=True) # Troba l'etiqueta "a" que conté el títol i el desa a "llibre"
-    titol = llibre["title"] # Desa a "titol" el contingut de l'atribut "title" de l'etiqueta "a"
-    preu = article.find("p", class_="price_color") # Troba l'etiqueta "p" que conté el preu
-    preu_final = preu.text # Agafa el text que hi ha dins l'etiqueta "p"
-    puntuacio = "" # Afegir la puntuació de cada llibre
-    if article.find("p", class_="star-rating One"):
-        puntuacio = "1/5"
-    elif article.find("p", class_="star-rating Two"):
-        puntuacio = "2/5"
-    elif article.find("p", class_="star-rating Three"):
-        puntuacio = "3/5"
-    elif article.find("p", class_="star-rating Four"):
-        puntuacio = "4/5"
-    elif article.find("p", class_="star-rating Five"):
-        puntuacio = "5/5"
-    else:
-        puntuacio = "Sense puntuació"   
-    print(f"{i}. {titol} | {preu_final[2:]} £ | Puntuació: {puntuacio}") # Elimino els dos primers caracters perquè el símbol de lliure d'origen no es codifi
-    nou_llibre = { # Crea el diccionari per a aquest llibre amb les tres claus
-        "titol": titol,
-        "preu": preu_final,
-        "puntuacio": puntuacio,
+def conversio(numero): # Funció per convertir una paraula en número. L'argument que agafa és la paraula.
+    equivalencies = { # Diccionari d'equivalències.
+        "One" : 1,        
+        "Two" : 2,
+        "Three" : 3,
+        "Four" : 4,
+        "Five" : 5,
     }
-    llibres.append(nou_llibre) # Afegeix el diccionari a la llista de llibres.
+    return(equivalencies[numero]) # Retorna el valor associat a la clau.
 
 try:
-    with open('llibres.json', 'w') as fitxer: ## Obre agenda.json en mode escriptura per actualitzar-lo.
-        json.dump(llibres, fitxer, ensure_ascii=False, indent=4)
-except PermissionError:
-    print("No tens permisos per desar el fitxer.")
-except OSError:
-    print("Error del sistema.")
+    response = requests.get("https://books.toscrape.com")
+    if response.status_code == 200: ## Si la resposta del servidor és positiva, executa el codi.
+        soup = BeautifulSoup(response.text, "html.parser")
+        elements = soup.find_all("article", class_="product_pod") # Retorna una llista amb tots els objectes "article".
+        llibres = []
+        print("Llibres:\n")
+        for i, article in enumerate(elements, start=1): # Recorre cada element de la llista
+            llibre = article.find("a", title=True) # Troba l'etiqueta "a" que conté el títol i el desa a "llibre"
+            titol = llibre["title"] # Desa a "titol" el contingut de l'atribut "title" de l'etiqueta "a"
+            preu = article.find("p", class_="price_color") # Troba l'etiqueta "p" que conté el preu
+            preu_final = preu.text # Agafa el text que hi ha dins l'etiqueta "p"
+            puntuacio = article.find("p")
+            estrelles = puntuacio["class"][1]
+            print(f"{i}. {titol} | {preu_final[2:]} £ | Puntuació: {conversio(estrelles)} estrelles") # Elimino els dos primers caracters perquè el símbol de lliure d'origen no es codifi
+            nou_llibre = { # Crea el diccionari per a aquest llibre amb les tres claus
+                "titol": titol,
+                "preu": preu_final[2:],
+                "puntuacio": conversio(estrelles),
+            }
+            llibres.append(nou_llibre) # Afegeix el diccionari a la llista de llibres.
+        try:
+            with open('llibres.json', 'w') as fitxer: ## Obre agenda.json en mode escriptura per actualitzar-lo.
+                json.dump(llibres, fitxer, ensure_ascii=False, indent=4)
+        except PermissionError:
+            print("No tens permisos per desar el fitxer.")
+        except OSError:
+            print("Error del sistema.")
+
+except ConnectionError: ## Si la connexió amb el servidor ha fallat, informa.
+    print("No s'ha pogut connectar amb el servidor")
